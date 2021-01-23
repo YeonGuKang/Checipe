57f9ad3a5c35733ef9d4b59b72c82ed84c556972
YeonGuKang <hsmroof@naver.com>
2021-01-23 21:42:17 +0900

    채식주의자 type에 따른 선택적 DB연결 및 보여줌

--- src/routes/Recipe.js
+++ src/routes/Recipe.js
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch, Link, BrowserRouter } from 'react-router-dom';
 import checipe_logo from './image/chaecipielogo.png';
-import image2 from './image/image2.jfif';
 import rec from "./Recipe.module.css";
-import { authService } from '../firebase';
-// react에서 slick을 사용하기 위한 import 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { authService , dbService } from '../firebase';
 import {ReactComponent as Msvg} from './image/menu.svg'
 
   
@@ -32,6 +27,108 @@ const Recipe = () => {
     
   }, []);
 
+  // 파이어베이스에서 데이터를 가져오는 과정
+  // 각각 채식 type에 맞게 데이터를 불러오기 위함
+    const [Lacto, setLacto] = useState([]);
+    const [LactoOvo, setLactoOvo] = useState([]);
+    const [Ovo, setOvo] = useState([]);
+    const [Pesco, setPesco] = useState([]);
+    const [Pollo, setPollo] = useState([]);
+    const [PolloPesco, setPolloPesco] = useState([]);
+    const [Flexi, setFlexi] = useState([]);
+    const [Vegan, setVegan] = useState([]);
+
+  // 사용자가 선택한 type에 맞게 보여주기위함
+    const [chosen, setchosen] = useState([]);
+
+    const getChecipes = async () =>
+    {
+      // 파이어베이스에 있는 컬렉션으름으로 각각의 db정보를 받아옴
+      const dbLacto = await dbService.collection("락토").get();
+      const dbLactoOvo = await dbService.collection("락토오보").get();
+      const dbOvo = await dbService.collection("오보").get();
+      const dbPesco = await dbService.collection("페스코").get();
+      const dbPollo = await dbService.collection("폴로").get();
+      const dbPolloPesco = await dbService.collection("폴로페스코").get();
+      const dbFlexi = await dbService.collection("플렉시테리언").get();
+      const dbVegan = await dbService.collection("비건").get();
+      
+      // dbLacto에 존재하는 모든 각각의 document에 대해서 실행
+      dbLacto.forEach((document) => {
+        const LactoObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        // Lacto 객체에 파이어베이스에 있는 정보를 set해줌 (set 함수인자에 함수를 넣어준 형태)
+        // prev => []  형태는 모든 이전의 document에 대해서 배열을 리턴한다
+        // 가장 최근 document인 Object를 보여주고 그 뒤로 이전 documnet를 보여줌
+        setLacto((prev) => [LactoObject, ...prev]);
+      });
+
+      dbLactoOvo.forEach((document) => {
+        const LactoOvoObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setLactoOvo((prev) => [LactoOvoObject, ...prev]);
+      });
+
+      dbOvo.forEach((document) => {
+        const OvoObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setOvo((prev) => [OvoObject, ...prev]);
+      });
+
+      dbPesco.forEach((document) => {
+        const PescoObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setPesco((prev) => [PescoObject, ...prev]);
+      });
+
+      dbPollo.forEach((document) => {
+        const PolloObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setPollo((prev) => [PolloObject, ...prev]);
+      });
+
+      dbPolloPesco.forEach((document) => {
+        const PolloPescoObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setPolloPesco((prev) => [PolloPescoObject, ...prev]);
+      });
+
+      dbFlexi.forEach((document) => {
+        const FlexiObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setFlexi((prev) => [FlexiObject, ...prev]);
+      });
+
+      dbVegan.forEach((document) => {
+        const VeganObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setVegan((prev) => [VeganObject, ...prev]);
+      });
+    }
+
+    
+    // 처음에 mount 됐을때 실행해줌으로써 데이터를 불러옴
+    useEffect(()=>{
+      getChecipes();
+ 
+    },[]);
+  
   // 로그아웃을 위한 함수를 선언
   const onLogOutClick = () => authService.signOut();
 
@@ -42,6 +139,43 @@ const Recipe = () => {
   //   nav.classList.toggle('active');
   //   login.classList.toggle('active');
   // });
+
+
+  // 사용자가 선택한 type에 맞게 데이터를 선택하는 함수
+      const getChosen = async (event) => {
+      const {
+        target: {name},
+      } = event;
+
+      // 아래 name으로 판단해서 chosen 객체에 앎맞는 데이터를 주입
+      if(name == "Lacto"){
+        setchosen(Lacto);
+      } 
+      else if(name == "Ovo"){
+        setchosen(Ovo);
+      }
+      else if(name == "LactoOvo"){
+        setchosen(LactoOvo);
+      }
+      else if(name == "Pollo"){
+        setchosen(Pollo);
+      }
+      else if(name == "Pesco"){
+        setchosen(Pesco);
+      }
+      else if(name == "PolloPesco"){
+        setchosen(PolloPesco);
+      }
+      else if(name == "Flexi"){
+        setchosen(Flexi);
+      }
+      else if(name == "Vegan"){
+        setchosen(Vegan);
+      }
+
+      console.log(chosen);
+    }
+
     return(            
             <div className={rec.wrap}>                            
               <nav className={rec.header}>                                  
@@ -74,8 +208,45 @@ const Recipe = () => {
                   <Msvg className></Msvg>
                 </a>
               </nav>
-              <div className={rec.half_bg} />               
-            </div>            
+              <div>
+
+          {/* 버튼을 클릭했을때 name의 값을 getChosen으로 넘겨줌 */}
+            <div>
+               <button onClick={getChosen} name="Lacto">Lacto</button>
+               <button onClick={getChosen} name="Ovo">Ovo</button>
+               <button onClick={getChosen} name="LactoOvo">LactoOvo</button>
+               <button onClick={getChosen} name="Pesco">Pesco</button>
+               <button onClick={getChosen} name="Pollo">Pollo</button>
+               <button onClick={getChosen} name="PolloPesco">PolloPesco</button>
+               <button onClick={getChosen} name="Flexi">Flexi</button>
+               <button onClick={getChosen} name="Vegan">Vegan</button>
+            </div>
+
+            {/* 사용자가 클릭한 type에 맞는 객체 정보를 쭉 나열해서 보여줌 */}
+            <div>
+              {chosen.map((Show)=>(
+                <div key={Show.id}>
+                  <h1>{Show.id}</h1>
+                  <img
+                    src={ Show.img }
+                    width='300px'
+                    height='300vh'
+                 />
+                  <h2>{Show.part}</h2>
+                  <h3>{Show.way}</h3>
+                  <h5>{Show.detail}</h5>
+                  <h6>{Show.number}</h6>
+                  </div>
+              ))}
+            </div>
+            
+          </div>            
+              <div className={rec.half_bg} />  
+            </div>
+            
+          
+            
+            
     );
 }         
 
