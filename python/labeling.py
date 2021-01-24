f19bd8f9eee0cf8ef63a8d2e001fc1b9d7ac2f6a
mini <jongmin4422@naver.com>
2021-01-24 12:57:23 +0900

    optimization labeling code.

--- python/labeling.py
+++ python/labeling.py
@@ -11,6 +11,7 @@ div_dic = dict()
 for key, value in zip(mining['ing'], mining['div']):
     div_dic[key] = value
 
+# 한 메뉴씩 접근
 for rcp in ing:
     temp = rcp.replace('\r', '').replace('\n', ',')
     edit_ingre = (rcp.split(','))
@@ -18,20 +19,33 @@ for rcp in ing:
     for d in edit_ingre:
         d = re.sub(r'\([^)]*\)', '', d)  # 괄호 안의 것 필터링
         d = re.sub(r'약간|적당량|다진것|\d.', '', d)  # 괄호 안의 것 필터링
-        spliting.append(hangul.sub(' ', d).strip())
+        # spliting.append(hangul.sub(' ', d).strip())
+        d = hangul.sub(' ', d).strip()
+        try:
+            spl = d.split(' ')
+            for a in spl:
+                spliting.append(a.strip())
+        except:
+            spliting.append(d.strip())
+    spliting = []
+    spliting_fin.append(spliting)
 
-for spl in spliting:
-    spl = spl.split(' ')
-    for a in spl:
-        spliting_fin.append(a.strip())
-
-dives = []
 print(spliting_fin)
-for spl in spliting_fin:
-    try:
-        dives.append(div_dic[spl])
-    except Exception:
-        print(spl + '는 없습니다.')
-    div_num.append(dives)
-print(dives)
-print(div_num)
+print(len(spliting_fin))
+# for spl in spliting:
+#     print(spl)
+#     spl = spl.split(' ')
+#     print(spl)
+#     for a in spl:
+#         spliting_fin.append(a.strip())
+
+# dives = []
+# print(spliting_fin)
+# for spl in spliting_fin:
+#     try:
+#         dives.append(div_dic[spl])
+#     except Exception:
+#         print(spl + '는 없습니다.')
+#     div_num.append(dives)
+# print(dives)
+# print(div_num)
