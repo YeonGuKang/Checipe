07195b9df65ad874db67d90c38fc1662257bbdea
kwjinwoo <jung003327@gmail.com>
2021-01-25 15:53:16 +0900

    data
    labeling

--- python/labeling.py
+++ python/labeling.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import re
 
-ing = pd.read_csv('data/data.csv', encoding='euc-kr')
-ing = ing.dropna(axis=0)
+ing = pd.read_csv('data/merge.csv')
 ing = ing['detail']
 div_num, spliting, spliting_fin = [], [], []
 
@@ -10,11 +9,12 @@ mining = pd.read_csv('data/mining_merge.csv', encoding='UTF-8')
 div_dic = dict()
 for key, value in zip(mining['ing'], mining['div']):
     div_dic[key] = value
+#div_dic[''] = 0
 
 # 한 메뉴씩 접근
 for rcp in ing:
     temp = rcp.replace('\r', '').replace('\n', ',')
-    edit_ingre = (rcp.split(','))
+    edit_ingre = (temp.split(','))
     hangul = re.compile('[^ \u3131-\u3163\uac00-\ud7a3]+')  # 한글 외 문자
     for d in edit_ingre:
         d = re.sub(r'\([^)]*\)', '', d)  # 괄호 안의 것 필터링
@@ -24,28 +24,30 @@ for rcp in ing:
         try:
             spl = d.split(' ')
             for a in spl:
+                if a == '':
+                    continue
                 spliting.append(a.strip())
         except:
             spliting.append(d.strip())
-    spliting = []
     spliting_fin.append(spliting)
+    spliting = []
 
-print(spliting_fin)
-print(len(spliting_fin))
-# for spl in spliting:
-#     print(spl)
-#     spl = spl.split(' ')
-#     print(spl)
-#     for a in spl:
-#         spliting_fin.append(a.strip())
 
-# dives = []
-# print(spliting_fin)
-# for spl in spliting_fin:
-#     try:
-#         dives.append(div_dic[spl])
-#     except Exception:
-#         print(spl + '는 없습니다.')
-#     div_num.append(dives)
-# print(dives)
-# print(div_num)
+dives = []
+
+for fspl in spliting_fin:
+    div_num = []
+    for sspl in fspl:
+        div_num.append(div_dic[sspl])
+    dives.append(list(set(div_num)))
+
+print(dives)
+div = pd.Series({'numbers': []})
+for v in dives:
+    
+print(div)
+
+
+'''merge = pd.read_csv('data/merge.csv')
+
+print(merge)'''
