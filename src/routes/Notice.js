2d48061695319b683b429a5475e9b64bc0a956b8
mini <jongmin4422@naver.com>
2021-01-31 18:38:29 +0900

    Make css file correspond to each js file

--- src/routes/Notice.js
+++ src/routes/Notice.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch, Link, BrowserRouter, Redirect } from 'react-router-dom';
 import checipe_logo from './image/chaecipielogo.png';
 import rec from "./Recipe.module.css";
+import noti from "./Notice.module.css";
 import { authService , dbService } from '../firebase';
 import {ReactComponent as Msvg} from './image/menu.svg'
-
 
 
 const Notice = () => {
@@ -18,6 +18,11 @@ const Notice = () => {
   const [boards, setboards] = useState([]);
   const [limit_boards,setlimit_boards]=useState([]);
 
+  // url 설정이 완료됐나 확인
+  const [check,setcheck] = useState(false);
+
+  // 선택한 게시글에 알맞는 url을 설정해주기 위함
+  const [View_url,setView_url] = useState("")
 
   // 현재 자신이 존재하는 페이지를 알기 위함
   const [page,setpage] = useState(1);
@@ -86,10 +91,24 @@ const Notice = () => {
 
 //  다시 그 temp 객체를 hook객체에 저장 (아래에 사용을 위해서 hook을 이용해야함)
  setlimit_boards(page_boards)
+ console.log("정보세팅" , limit_boards)
 
 }
 
+console.log("함수밖 정보세팅" , limit_boards)
+  // 게시글에 맞는 url을 설정해주는 함수
+  const setUrl = async (event) => {
+    // event안에 존재하는 target의 value를 name으로 넘긴다.
+  const {
+    target: {name},
+  } = event;
 
+  // url에 /View/ 와 전달받은 name을 붙임 name은 doc.id이다
+ setView_url('/View/' + name);
+
+//  url설정이 완료됐음을 설정
+  setcheck(true);
+}
 
 
     // 로그아웃을 위한 함수를 선언
@@ -134,16 +153,19 @@ const Notice = () => {
           <li><Link to="/Register">글 등록하기</Link></li>
 
         {/* 게시글을 보여주기 위한 middle 부분 */}
-          <div className = {rec.middle}>
-            <div className={rec.board}>
-                <div  className={rec.board_detail}>
+          <div className = {noti.middle}>
+            <div className={noti.board}>
+                <div  className={noti.board_detail}>
                   {/* 제목 부분에 title을 불러옴 */}
                     <div>
                         제목
                         {limit_boards.map(board => 
                         <div key={board.id}>
                           {/* 현재 버튼이 아니면 인식이 안돼서 일단 버튼으로 만들어 놓음 name에 id를 넘겨줌 */}
-                            <li><Link to = {"/View/" + board.id}>{board.title}</Link></li>
+                            <button onClick={setUrl} name={board.id}>{board.title}</button>
+                            {/* url 설정이 완료됐으면 그 url로 redirect */}
+                            <div>{check ? <Redirect from="*" to = {View_url} />: null}
+                        </div>
                         </div>
                      
                        )
@@ -151,7 +173,7 @@ const Notice = () => {
               
                     </div>
                     {/* 마찬가지로 날짜 부분에 만든 날짜를 게시글 작성 날짜를 불러옴 */}
-                    <div className={rec.board_date}>
+                    <div className={noti.board_date}>
                         날짜
                         {console.log("정보출력" , limit_boards)}
                         {limit_boards.map(board => 
@@ -162,13 +184,13 @@ const Notice = () => {
                     </div>
                     </div>
                 {/* 페이징을 위한 부분 */}
-                    <div className={rec.paging_div}>
+                    <div className={noti.paging_div}>
          
          {/* 페이지 개수에 맞게 페이지 번호를 만들어주고 클릭시에 그 페이지에 맞는 게시글을 보여줌 */}
               <div>
                 {page_arr ? page_arr.map( (el,key) => 
-                    el == page ? <button key={key} className={rec.page_num} onClick={getpage} name={el}> {el} </button>
-                                : <button key={key} className={rec.page_num} onClick={getpage} name={el}> {el}  </button> 
+                    el == page ? <button key={key} className={noti.page_num} onClick={getpage} name={el}> {el} </button>
+                                : <button key={key} className={noti.page_num} onClick={getpage} name={el}> {el}  </button> 
                 )
                 
                 : null}
