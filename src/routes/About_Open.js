4169f14d378f7cd348d1d4b94c11c386b6af975f
MINI <koreajongmin72024@gmail.com>
2021-02-25 19:40:47 +0900

    add about (in progress)

--- src/routes/About_Open.js
+++ src/routes/About_Open.js
@@ -38,12 +38,17 @@ class About_Open extends React.Component {
            {/* 게시글을 보여주기 위한 middle부분 */}
            <div className = {viewb.middle}>
             <div className = {viewb.middlewh}>
-                 <div className={viewb.vtitle}></div>
+                 <div className={viewb.vtitle}>채식에 관하여</div>
                  <hr size='5' color='#28421f'></hr> 
                  <div className={viewb.vdate}></div>
                  <hr size='2' color='#28421f'></hr> 
  
-                 <textarea readOnly className={viewb.vcontent}></textarea>
+                 <textarea readOnly className={viewb.vcontent}>                  
+                  {`채식 소개 | 채식주의자의 정의
+채식 현황 | 우리나라에서 채식주의자들의 증가가 나타나고 있다.
+채식의 장점
+                  `}
+                 </textarea>
                  </div>
              <div>
          </div>
